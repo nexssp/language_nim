8070284a90157c3a1880ef34762c78343f1fc6e3
Marcin Polak <mapoart@gmail.com>
2020-09-23 18:48:00 +0200

    gitignore

--- nim.linux.nexss.config.js
+++ nim.linux.nexss.config.js
@@ -1,42 +1,34 @@
 let languageConfig = Object.assign({}, require(`./nim.win32.nexss.config`));
-
-let sudo = "sudo ";
-if (process.getuid && process.getuid() === 0) {
-  sudo = "";
-}
+const os = require("@nexssp/os");
+let sudo = os.sudo();
 
 languageConfig.compilers = {
   nim: {
-    install: `${sudo} apt-get install nim`,
+    install: `${sudo}apt-get install nim`,
     command: "nim",
     args: "compile --verbosity:0 --hints:off --run <file>", //&& del <fileNoExt>.exe
     help: ``,
   },
 };
 
-const {
-  replaceCommandByDist,
-  dist,
-} = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
-
-const distName = dist();
+const distName = os.name();
 languageConfig.dist = distName;
 
 // TODO: Later to cleanup this config file !!
 switch (distName) {
-  case "Alpine Linux":
-    languageConfig.compilers.nim.install = `${sudo}apk add g++ curl tar xz nodejs
-${sudo}mkdir -p /nim
-${sudo}curl -sL "http://nim-lang.org/download/nim-1.2.6.tar.xz"|tar xJ --strip-components=1 -C /nim
-${sudo}cd /nim
-bash build.sh
-${sudo}rm -r c_code tests
-${sudo}ln -s /nim/bin/nim /bin/nim`;
-    break;
-  default:
-    languageConfig.compilers.nim.install = replaceCommandByDist(
+  case os.distros.UBUNTU:
+    languageConfig.compilers.nim.install = os.replacePMByDistro(
       languageConfig.compilers.nim.install
     );
+    break;
+  default:
+    languageConfig.compilers.nim.install = os.replacePMByDistro(`${sudo}apt-get install -y gcc g++ curl tar xz nodejs
+${sudo}mkdir -p /nim
+${sudo}curl -sL "http://nim-lang.org/download/nim-1.2.6.tar.xz"|tar xJ --strip-components=1 -C /nim
+cd /nim
+${sudo}bash build.sh
+${sudo}rm -r c_code tests
+${sudo}ln -sf /nim/bin/nim /bin/nim`);
     break;
 }
 
