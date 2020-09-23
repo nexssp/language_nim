e89b83e90e19bd0582af1f214c9c77535d0b622d
Marcin Polak <mapoart@gmail.com>
2020-09-23 02:21:22 +0200

    Alpine

--- nim.linux.nexss.config.js
+++ nim.linux.nexss.config.js
@@ -24,6 +24,15 @@ languageConfig.dist = distName;
 
 // TODO: Later to cleanup this config file !!
 switch (distName) {
+  case "Alpine Linux":
+    languageConfig.compilers.nim.install = `${sudo}apk add g++ curl tar xz nodejs
+${sudo}mkdir -p /nim
+${sudo}curl -sL "http://nim-lang.org/download/nim-1.2.6.tar.xz"|tar xJ --strip-components=1 -C /nim
+${sudo}cd /nim
+bash build.sh
+${sudo}rm -r c_code tests
+${sudo}ln -s /nim/bin/nim /bin/nim`;
+    break;
   default:
     languageConfig.compilers.nim.install = replaceCommandByDist(
       languageConfig.compilers.nim.install
